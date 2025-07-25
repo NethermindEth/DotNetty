2bacd323be6b71c834db2c63796d9935815b293f
MarekM25 <marekm2504@gmail.com>
2025-07-25 13:18:40 +0200

    increase it even more

--- src/DotNetty.Transport/Channels/Sockets/DefaultDatagramChannelConfig.cs
+++ src/DotNetty.Transport/Channels/Sockets/DefaultDatagramChannelConfig.cs
@@ -11,7 +11,7 @@ namespace DotNetty.Transport.Channels.Sockets
 
     public class DefaultDatagramChannelConfig : DefaultChannelConfiguration, IDatagramChannelConfig
     {
-        const int DefaultFixedBufferSize = 4096;
+        const int DefaultFixedBufferSize = 10240;
 
         readonly Socket socket;
 
